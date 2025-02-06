19061a547a5f3748886b321c1670665817ec3204
Ryan Sarles <ryan.sarles@contractor.tranetechnologies.com>
2025-02-06 12:46:59 -0500

    add sub id to provider

--- examples/providers.tf
+++ examples/providers.tf
@@ -10,7 +10,7 @@ terraform {
 provider "azurerm" {
   features {
   }
-  subscription_id = var.subscription_id
+  subscription_id = "d003ea48-de34-4898-8ad4-daa31fd6479c"
   tenant_id       = "9ca75128-a244-4596-877b-f24828e476e2"
 
 }
